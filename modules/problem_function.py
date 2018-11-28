c44118ea6289c83aa8d95202881ed41607917a94
Daniel Topping <42147338+DTopping256@users.noreply.github.com>
2018-11-28 14:50:13 +0000

    Put the problem1 function and differential into a module

---
+++ modules/problem_function.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import math
+e = math.e
+
+# The python definitions of f(x) & f'(x) for problem 1.
+
+def f(x):
+    if (x <= 100):
+        return float(-e**(-(x/100)**2))
+    else:
+        return float(-e**(-1) + (x - 100)*(x - 102))
+    
+def diff_f(x):
+    if (x <= 100):
+        return float(e**(-(x/100)**2)/5000)
+    else:
+        return float(2*(x - 101))
